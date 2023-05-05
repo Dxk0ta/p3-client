7b290db924a14a3bc5fa1b8bee03d29dd8bea27f
juanedcabrera <juanedcabrera@gmail.com>
2023-05-05 17:06:44 -0600

    Favorite button post, check work, not delete

--- src/components/partials/FavoritesButton.jsx
+++ src/components/partials/FavoritesButton.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function FavoritesButton(props) {
-  const [isFavorite, setIsFavorite] = useState(false); //I MIGHT NOT NEED props.isfavorite - change to false instead
+const FavoritesButton = ({ movie }) => {
+  console.log(movie)
+  const [isFavorite, setIsFavorite] = useState(false);
   const jwt = localStorage.getItem("jwt");
   const tmdbId = `${movie.id}`
 
@@ -43,49 +44,27 @@ export default function FavoritesButton(props) {
         });
         const favorites = response.data.result;
         const ids = favorites.map(favorite => favorite.id)
+        console.log(ids)
+
+        console.log(`Sanity`, ids.includes(tmdbId), tmdbId)
         setIsFavorite(ids.includes(tmdbId));
       } catch (err) {
         console.log(err);
       }
     };
-    // get all objectid from users favorites
-    // get all 
+  // get all objectid from users favorites
+  // get all 
+
+
+
     checkFavorite();
   }, [jwt, tmdbId]);
-
-  const toggleFavorite = async () => {
-    try {
-      navigate(0)
-      if (isFavorite) {
-        // remove from favorites
-        await axios.delete(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/favorites/${props.objectId}`, {
-          headers: {
-            Authorization: `${jwt}`,
-          },
-        });
-        setIsFavorite(false);
-      } else {
-        // add to favorites
-        const sendData = { ...props.movie, userId: currentUser._id }
-        await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/favorites`, sendData, {
-          headers: {
-            Authorization: `${jwt}`,
-          },
-        });
-        setIsFavorite(true);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-
-
-
+  
+  
 
   return (
-    <button type="button" class="btn btn-sm font-weight-bold" onClick={toggleFavorite}>
-      {isFavorite ? "Remove From Favorites" : "Add To Favorites"}
+    <button onClick={toggleFavorite}>
+      {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
     </button>
   );
 };
