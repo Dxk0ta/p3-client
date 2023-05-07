8348c807516c999b65919e74a9679197d4c2c4ba
dxk0ta <dxkotacamps@gmail.com>
2023-05-07 01:34:01 -0700

    styling details page

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ function App() {
           <Route path="/search/*" element={<SearchMovies />} />
         </Routes>
       </div>
-      <Footer />
+      <Footer className="footer"/>
     </Router>
   );
 }
