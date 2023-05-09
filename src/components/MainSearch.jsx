10a97bce796b37f7edabdc83ccd961bf0ec26cc9
mylesw27 <myleswiegel27@gmail.com>
2023-05-08 20:09:29 -0700

    Add current user to header

---
+++ src/components/MainSearch.jsx
@@ -0,0 +1,36 @@
+// import react and the necessary hoooks
+import { useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+// define mainsearch component
+function MainSearch() {
+  // set up state variables
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  let navigate = useNavigate()
+
+  // define handle
+  const handleSearch = (event) => {
+    event.preventDefault();
+    navigate(`/search/?q=${searchQuery}`, { replace: false })
+    navigate(0)
+    setSearchQuery('')
+  };
+
+  return (
+    <div className="main-search">
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(event) => setSearchQuery(event.target.value)}
+        />
+        <button className="main-search-button" type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default MainSearch;
